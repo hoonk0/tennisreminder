7e11b46e324566deff1202dd0893ebc706332226
hoonk0 <karlhkim1@gmail.com>
2025-07-19 18:42:13 +0900

    android ver7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 6 ///버전관리
+        versionCode = 7 ///버전관리
         versionName = "3.0.0"
     }
 
