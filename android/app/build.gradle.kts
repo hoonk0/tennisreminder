6dd0100a85bb3c0dd11c6c82ecf9f741a3602c6c
hoonk0 <karlhkim1@gmail.com>
2025-07-23 09:51:40 +0900

    ver11 update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 10 ///버전관리
+        versionCode = 11 ///버전관리
         versionName = "3.0.0"
     }
 
